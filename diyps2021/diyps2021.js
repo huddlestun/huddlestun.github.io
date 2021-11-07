bb353ef60ede204c88ffdbd2fcc0e2e727593919
huddlestun <91921483+huddlestun@users.noreply.github.com>
2021-11-07 01:26:21 -0700

    Update diyps2021.js

--- diyps2021/diyps2021.js
+++ diyps2021/diyps2021.js
@@ -6,15 +6,15 @@ var screenbg = 'black'; // black background
 
 function preload() {
 // preload() runs once, it may make you wait
-// img = loadImage('demon_eye.jpg');  // cat.jpg needs to be next to this .js file
+// img = loadImage('demon_eye.jpg');  // demon_eye.png needs to be next to this .js file
 // you can link to an image on your github account
-img = loadImage('demon_eye.png');
-img2 = loadImage('angel_eye.png');
-img3 = loadImage('redflames.png');
-img4 = loadImage('blueflames.png');
-img5 = loadImage('star.png');
-img6 = loadImage('blackstar.png');
-img7 = loadImage('candle.png');
+img = loadImage('demon_eye.png'); //used for key 7
+img2 = loadImage('angel_eye.png'); //used for key 6
+img3 = loadImage('redflames.png'); //used for key 4
+img4 = loadImage('blueflames.png'); //used for 3
+img5 = loadImage('star.png'); //used for key 8
+img6 = loadImage('blackstar.png'); //used for key 9
+img7 = loadImage('candle.png'); // used for key g
 //img=loadImage('https://huddlestun.github.io/diyps2021/demon_eye.png');
 //img2= loadImage('https://huddlestun.github.io/diyps2021/angel_eye.png');
 //img3= loadImage('https://huddlestun.github.io/diyps2021/redflames.png');
@@ -47,42 +47,41 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
  if (toolChoice == '1' ) {  // first tool
    stroke('blue'); //white rays
    fill('black'); //grey fill
-   triangle(mouseX, mouseY, 300, 200, pmouseX,pmouseY); //anchored @ center for now
+   triangle(mouseX, mouseY, 300, 200, pmouseX,pmouseY); //anchored @ center for adjustable radial design
   } else if (toolChoice == '2') { // second tool
-
    strokeWeight(0.5);
    stroke('red'); //red lines defining rays
    fill('black'); //black fill between red rays
-    triangle(mouseX, mouseY, 300, 200, pmouseX,pmouseY); //anchored @ center for now
+    triangle(mouseX, mouseY, 300, 200, pmouseX,pmouseY); //anchored @ center for adjustable radial design
   } else if (toolChoice == '3') { // third tool
 
-     image(img4, mouseX-25, mouseY-25, 50, 50);
+     image(img4, mouseX-25, mouseY-25, 50, 50); //insert blue flames
   } else if (toolChoice == '4') {
       
-    image(img3, mouseX-25, mouseY-25, 50, 50);
+    image(img3, mouseX-25, mouseY-25, 50, 50); //insert red flames
   } else if (key == '5') { // this tool calls a function
-    stroke(0);
-    testbox(0, 255, 255);
-    testbox(200, 500, 20, 0);
+    stroke(0); //set to black
+    testbox(200, 500, 20, 0); //set neon green
  // make testbox do something!
  //   line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '6') {
 
-    image(img2, mouseX-25, mouseY-25, 50, 50);
+    image(img2, mouseX-25, mouseY-25, 50, 50); //insert angel eye
   } else if (toolChoice == '7') {
        
-    image(img, mouseX-25, mouseY-25, 50, 50);
+    image(img, mouseX-25, mouseY-25, 50, 50); //insert demon eye
   } else if (toolChoice == '8') {
 
-    image(img5, mouseX-25, mouseY-25, 50, 50);
+    image(img5, mouseX-25, mouseY-25, 50, 50); //insert white outline star
   } else if (toolChoice == '9') {
-    image(img6, mouseX-25, mouseY-25, 50, 50);
-  } else if (toolChoice == '0') {
-    stroke(0, 0, 0, 0);
+    
+    image(img6, mouseX-25, mouseY-25, 50, 50); // insert black star filled in
+  } else if (toolChoice == '0') { //low opacity black squares brush for shading
+    stroke(0, 0, 0, 0); 
     fill(0, 0, 0, 10);
    rect(mouseX, mouseY, 100, 100);
   } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-   image(img7, mouseX-25, mouseY-25, 50, 50);
+   image(img7, mouseX-25, mouseY-25, 50, 50); //insert candle
   } 
  }
  
@@ -91,7 +90,7 @@ function testbox(r, g, b) {
   x = mouseX;
   y = mouseY;
   fill(r, g, b);
-  rect(x-10, y-10, 20, 20);
+  rect(x-10, y-10, 20, 20); //set size for key 5 squares
 
 }
 
